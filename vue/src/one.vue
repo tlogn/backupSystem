12f7e64f82664f4843580aeb7e3b55dd6354a59b
956077450 <956077450@qq.com>
2021-09-07 13:15:07 +0800

    upd

--- vue/src/one.vue
+++ vue/src/one.vue
@@ -1,28 +1,54 @@
 <template>
   <div id="one">
-    <h1>Backup</h1>
+    <h1>TEST</h1>
+    <hr />
     <br />
-    <textarea
-      v-model="params"
-      placeholder="请输入参数..."
-      style="height: 80px; width: 400px; font-size: 18px"
-    />
-    <br />
-    <p style="font-size:20px">http://localhost:8090/method</p>
-    <input
-      v-model="addr"
-      placeholder="请输入URL..."
-      style="height: 19px; width: 400px; font-size: 18px"
-    />
-    <button
-      @click="post(addr, params)"
-      style="height: 28px; width: 60px; font-size: 18px"
-    >
-      提交
-    </button>
-    <br><br>
-    <hr>
-    <p style="font-size:20px">reponse = {{ response_msg }}</p>
+    <div id="first">
+      <textarea
+        v-model="post_params"
+        placeholder="请输入POST参数..."
+        style="height: 80px; width: 400px; font-size: 18px"
+      />
+      <br />
+      <p style="font-size: 20px">http://localhost:8090/method</p>
+      <input
+        v-model="post_addr"
+        placeholder="请输入URL..."
+        style="height: 19px; width: 400px; font-size: 18px"
+      />
+      <button
+        @click="post(post_addr, post_params)"
+        style="height: 28px; width: 65px; font-size: 18px"
+      >
+        POST
+      </button>
+      <br />
+      <p style="font-size: 20px">status = {{ post_status }}</p>
+      <p style="font-size: 20px">reponse = {{ post_response_msg }}</p>
+    </div>
+    <div id="second">
+      <textarea
+        v-model="get_params"
+        placeholder="请输入GET参数..."
+        style="height: 80px; width: 400px; font-size: 18px"
+      />
+      <br />
+      <p style="font-size: 20px">http://localhost:8090/method</p>
+      <input
+        v-model="get_addr"
+        placeholder="请输入URL..."
+        style="height: 19px; width: 400px; font-size: 18px"
+      />
+      <button
+        @click="get(get_addr, get_params)"
+        style="height: 28px; width: 65px; font-size: 18px"
+      >
+        GET
+      </button>
+      <br />
+      <p style="font-size: 20px">status = {{ get_status }}</p>
+      <p style="font-size: 20px">reponse = {{ get_response_msg }}</p>
+    </div>
   </div>
 </template>
 
@@ -33,23 +59,48 @@ export default {
   data() {
     return {
       msg: "Backup",
-      response_msg: null
+      post_response_msg: null,
+      get_reponse_msg: null,
+      post_status: null,
+      get_status: null,
     };
   },
   methods: {
-    post: function (addr, params) {
+    post: function (addr, param) {
       if (addr == null) {
         window.alert("Empty URL");
       } else {
-        var that = this
+        var that = this;
         axios
-          .post(addr, { params })
+          .post(addr, { param })
           .then(function (response) {
-            that.response_msg = response
+            that.post_response_msg = response;
+            that.post_status = response.status
           })
           .catch(function (error) {
-            that.response_msg = null
-            window.alert(error)
+            that.post_response_msg = null;
+            window.alert(error);
+          });
+      }
+    },
+    get: function (addr, body) {
+      if (addr == null) {
+        window.alert("Empty URL");
+      } else {
+        var that = this;
+        axios
+          .get(addr, {
+            params: {
+              body,
+            },
+          })
+          .then(function (response) {
+            that.get_response_msg = response;
+            that.get_status = response.status
+          })
+          .catch(function (error) {
+            that.get_response_msg = null;
+            window.alert(error);
           });
       }
     },
@@ -64,6 +115,16 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2e659b;
-  margin-top: 100px;
+  margin-top: 40px;
+}
+#first {
+  width: 48%;
+  float: left;
+}
+#second {
+  width: 48%;
+  float: right;
+  /*height: 100px;
+  border: 1px solid #3b6273;*/
 }
 </style>
