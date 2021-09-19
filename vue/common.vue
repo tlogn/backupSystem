62cc1989e31b7e8001a66d7005a72e02685d71b9
956077450 <956077450@qq.com>
2021-09-20 00:33:43 +0800

    upd

---
+++ vue/common.vue
@@ -0,0 +1,45 @@
+
+<script>
+const Body = {
+  op: "",
+  dir_para: {
+    dir_path: "",
+  },
+
+  user_name: "",
+
+  login_para: {
+    username: "",
+    password: "",
+  },
+
+  copy_para: {
+    origin_path: "",
+    backup_path: "",
+  },
+
+  recover_para: {
+    recover_path: "",
+  },
+
+  compress_para: {
+    is_compress: false,
+    compress_path: "",
+  },
+
+  encode_para: {
+    is_encode: false,
+    encode_path: "",
+    password: "",
+  },
+
+  pack_para: {
+    is_pack: false,
+    pack_path: "",
+  },
+};
+
+export default {
+  Body,
+};
+</script>
