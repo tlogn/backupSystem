af10e236e1fc47f586875e72c86fef4bd7e4ebf4
yangzhouqike <yangzhouqike@bytedance.com>
2021-09-21 16:56:16 +0800

    compress finished v1
    Change-Id: I75b0d0e6eb50bd2a9179c00acbc4d8b7edae2c94

--- compress/compress_test.go
+++ compress/compress_test.go
@@ -3,5 +3,7 @@ package compress
 import "testing"
 
 func TestCompress(t *testing.T) {
-	buildTree("/Users/bytedance/y0x'sbproject/backupSystem/compress/train.txt")
+	//buildTree("/Users/bytedance/y0x'sbproject/backupSystem/compress/train.txt")
+	//compress("/Users/bytedance/go_test.sum")
+	undoCompress("/Users/bytedance/go_test.sum.ylx")
 }
